0081a1dc37f0b27678cf596397c00e653c00c496
XBlueSky <30610447+XBlueSky@users.noreply.github.com>
2017-08-16 21:32:28 +0800

    Add files via upload

---
+++ src/Bidder.java
@@ -0,0 +1,78 @@
+public class Bidder {
+    private int bid;
+    private float bidPerNum,critical;
+    private String [] bundle;
+    private String name;
+
+    public Bidder (String name){
+        setname(name);
+    }
+
+    public void setname(String name){
+        this.name = name;
+    }
+
+    public void setbid(int bid){
+        this.bid = bid;
+    }
+
+    public void setbundle(String bundle){
+        this.bundle = bundle.split(",");
+        this.bidPerNum = (float)bid / this.bundle.length;
+    }
+
+    public void setcritical(float critical){
+        this.critical = critical;
+    }
+
+    public String getname(){
+        return this.name;
+    }
+
+    public int getbid(){
+        return this.bid;
+    }
+
+    public String[] getbundle(){
+        return this.bundle;
+    }
+
+    public float getcritical(){
+        return this.critical;
+    }
+
+    public float getbidPerNum(){
+        return this.bidPerNum;
+    }
+
+    public static boolean disjointSet(String[] A,String[] B){
+        for(String itemA : A)
+            for(String itemB : B)
+                if(itemA.equals(itemB)) return false;
+
+        return true;
+    }
+
+	public static Bidder max(Bidder[] players){
+		Bidder max = new Bidder("max");
+
+		for(Bidder player : players)
+			if(player.getbidPerNum() > max.getbidPerNum())
+				max = player;
+
+		return max;
+	}
+
+    public static void sort(Bidder[] players){
+        Bidder temp = new Bidder("temp");
+        for(int i=0;i<(players.length-1);i++)
+            for(int j=0;j<(players.length-i-1);j++)
+                if(players[j].getbidPerNum()<players[j+1].getbidPerNum()){
+                    temp = players[j];
+                    players[j] = players[j+1];
+                    players[j+1] = temp;
+                }
+    }
+
+
+}
---
+++ src/Greedy_Allocation.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Greedy_Allocation {
+
+
+    public static void main(String[] args) {
+
+        //declartion
+        int bidder_num,bid;
+        String bundle;
+        Bidder players[];
+        ArrayList<Bidder> winner = new ArrayList<Bidder>();
+
+        //input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please input bidder's number ? ");
+        bidder_num = input.nextInt();
+        players = new Bidder[bidder_num];
+
+        for(int i=0;i<bidder_num;i++){
+            players[i] = new Bidder("P" + (i+1));
+            System.out.print("Input P" + (i+1) + "'s bid ? ");
+            bid = input.nextInt();
+            players[i].setbid(bid);
+            System.out.print("Input p" + (i+1) + "'s bundle ? ");
+            bundle = input.next();
+            players[i].setbundle(bundle);
+        }
+
+//        players = new Bidder[5];
+//        players[0] = new Bidder("P1");
+//        players[0].setbid(63);
+//        players[0].setbundle("A,C,D");
+//        players[1] = new Bidder("P2");
+//        players[1].setbid(54);
+//        players[1].setbundle("A,B,C");
+//        players[2] = new Bidder("P3");
+//        players[2].setbid(93);
+//        players[2].setbundle("B,D,E");
+//        players[3] = new Bidder("P4");
+//        players[3].setbid(70);
+//        players[3].setbundle("D,E");
+//        players[4] = new Bidder("P5");
+//        players[4].setbid(28);
+//        players[4].setbundle("A,C");
+
+        Bidder.sort(players);
+
+//        for(int i=0;i<players.length;i++)
+//            System.out.println(players[i].getname()+" : "+players[i].getbidPerNum());
+
+        winner.add(Bidder.max(players));
+
+        for(int i=0;i<players.length;i++){
+            boolean flag = true;
+            for(int j=0;j<winner.size();j++)
+                if(!Bidder.disjointSet(winner.get(j).getbundle(),players[i].getbundle())){
+                    flag = false;
+                    break;
+                }
+            if(flag)
+                winner.add(players[i]);
+        }
+
+//        for(int i=0;i<winner.size();i++)
+//            System.out.println(winner.get(i).getname());
+
+
+        for(int i=0;i<players.length;i++){
+            for(int j=0; j<winner.size();j++){
+                if(players[i].getname().equals(winner.get(j).getname())){
+                    for(int q = i+1;q<players.length;q++)
+                        if(!Bidder.disjointSet(players[i].getbundle(),players[q].getbundle())){
+                            players[i].setcritical(players[q].getbidPerNum()*players[i].getbundle().length);
+                            break;
+                        }
+                }
+                else{
+                    if(!Bidder.disjointSet(players[i].getbundle(),winner.get(j).getbundle())){
+                        players[i].setcritical(winner.get(j).getbidPerNum()*players[i].getbundle().length);
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println("******************* Critical Value *******************");
+        for(Bidder player : players)
+            System.out.println(player.getname() + " : " + player.getcritical());
+
+        System.out.println("******************* Result *******************");
+        for(int i=0;i<winner.size();i++)
+            System.out.println(winner.get(i).getname() + " should pay " + winner.get(i).getcritical());
+        //System.out.println(player.getbid() + " : " + Arrays.toString(player.getbundle()) + " : " + player.getbidPerNum());
+
+
+    }
+
+
+}
